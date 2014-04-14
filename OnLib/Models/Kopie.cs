a3d76a3462d6b88261e27e086268135387d08736
twerner93 <twerner93@users.noreply.github.com>
2014-04-14 18:49:46 +0200

    Web.config ConnectionString changed

--- OnLib/Models/Kopie.cs
+++ OnLib/Models/Kopie.cs
@@ -9,9 +9,9 @@ namespace OnLib.Models
     {
         public int Id { get; set; }
         public int TitelId { get; set; }
-        //public int BenutzerId { get; set; }
+        public int BenutzerId { get; set; }
         public string Typ { get; set; }
-        public string Ausgabe { get; set; } //Extended Cut oder sowas
+        public string Ausgabe { get; set; }
         public string Qualitaet { get; set; }
 
         public virtual Titel Titel { get; set; }
