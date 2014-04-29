2e749d0682e70b8930294293efb2b1297e66e380
twerner93 <twerner93@users.noreply.github.com>
2014-04-29 13:03:19 +0200

    fehlende Dateien hinzugefügt

---
+++ OnLib.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnLib;
+using OnLib.Controllers;
+using System.Threading.Tasks;
+
+namespace OnLib.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTest
+    {
+        //[ClassCleanup]
+        //public void RemoveTestUsers()
+        //{
+
+        //}
+
+        [TestMethod]
+        public void RegisterTest()
+        {
+            AccountController controller = new AccountController();
+
+            Assert.IsTrue(controller.UserExists("TestBenutzer123"));
+        }
+    }
+}
