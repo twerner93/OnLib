c96a11f421b2ed277dc54937c552ee66f890f73e
twerner93 <twerner93@users.noreply.github.com>
2014-05-07 15:47:13 +0200

    fehlende Dateien vom Leihe hinzugefügt

---
+++ OnLib/Models/Leihe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnLib.Models
+{
+    public class Leihe
+    {
+        public int LeiheId { get; set; }
+        public int KopieId { get; set; }
+        public ApplicationUser UserProfile { get; set; }
+        public DateTime Ausgeliehen { get; set; }
+        
+        [Display(Name = "Rückgabe")]
+        public DateTime Rueckgabe { get; set; }
+
+        [Display(Name = "Zurück")]
+        public bool Zurueck { get; set; }
+
+        public virtual Kopie Kopie { get; set; }
+    }
+}
