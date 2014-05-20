03d4cdee26571cf2ab580b4ac1b69cca1e86e1e5
twerner93 <twerner93@users.noreply.github.com>
2014-05-20 23:31:51 +0200

    Titel können Ausgeliehen werden; Weiterleitung funktioniert noch nicht, Eingabe ist unschön

--- OnLib/Controllers/KopieController.cs
+++ OnLib/Controllers/KopieController.cs
@@ -139,8 +139,14 @@ namespace OnLib.Controllers
         }
 
         // GET: /Kopie/Leihen/5
-        public ActionResult Leihen(int id)
+        public ActionResult Leihen(int? id)
         {
+            if (id == null)
+            {
+                ViewBag.KopieId = new SelectList(db.Kopies, "KopieId", "Name");
+                ViewBag.forKopie = false;
+                return View();
+            }
             Kopie kopie = db.Kopies.Find(id);
             var currentUserId = User.Identity.GetUserId();
             Leihe leihe = new Leihe
@@ -149,6 +155,7 @@ namespace OnLib.Controllers
                 KopieId = kopie.Id,
                 UserProfile = db.Users.Find(currentUserId)
             };
+            ViewBag.ForKopie = true;
             return View(leihe);
         }
 
